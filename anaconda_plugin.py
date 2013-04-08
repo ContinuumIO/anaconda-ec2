d69c9b6e0057dd86396a1622c8a8da23cadc85ed
Benjamin Zaitlen <quasiben@gmail.com>
2013-04-07 19:30:50 -0500

    updated plugin for new anaconda and change users in config template

--- anaconda_plugin.py
+++ anaconda_plugin.py
@@ -14,70 +14,67 @@ class ConfigAnaconda(ClusterSetup):
         Create the disco directories on the ephemeral storage.
         """
         if node == master:
-            node.ssh.execute("touch /media/test-master")
+            node.ssh.execute("touch /mnt/test-master")
         else:
             pass
-            node.ssh.execute("touch /media/test-node")
-            
+            node.ssh.execute("touch /mnt/test-node")
+
     def config_disco(self, master, nodes):
         """
         Create a disco config file that includes all the nodes and restart disco.
         """
         remote_nodes = filter(lambda n: not n.is_master(), nodes)
-        #names = map(lambda n: n.alias, remote_nodes)
-        recs = map(lambda n: ',\n["%s", "%d"]' % (n.alias, n.num_processors), remote_nodes)
+
+        names = map(lambda n: n.alias, remote_nodes)
+        recs = map(lambda x: ',\n["%s", "1"]' % x, names)
         body = reduce(lambda x, y: x + y, recs, '')
-        dconfig = '[["master", "%d"]' % (master.num_processors,) + body + ']'
+        dconfig = '[["master", "1"]' + body + ']'
 
         cfd = master.ssh.remote_file(DISCO_CONFIG)
         cfd.write(dconfig)
         cfd.close()
+
+        master.ssh.execute("disco stop")
         
-        master.ssh.execute("/etc/init.d/disco stop")
-        master.ssh.execute("/etc/init.d/disco start")
-    
     def config_anaconda(self, master, nodes):
             """
             Create a disco config file that includes all the nodes and restart disco.
             """
             remote_nodes = filter(lambda n: not n.is_master(), nodes)
             names = map(lambda n: n.alias, remote_nodes)
-            #recs = map(lambda n: ',\n["%s", "%d"]' % (n.alias, n.num_processors), remote_nodes)
-            #body = reduce(lambda x, y: x + y, recs, '')
+            recs = map(lambda x: ',\n["%s", "1"]' % x, names)
+            body = reduce(lambda x, y: x + y, recs, '')
             #master.ssh.execute("sudo su - disco;")
-            master.ssh.execute("/etc/init.d/disco stop")
-            
+            master.ssh.execute("/opt/anaconda/bin/disco stop")
+
+
+            master.ssh.execute("disco start")
+
             for node in names:
                 print node
-                print "scp ~disco/.ssh/id_rsa.pub %s:~disco/.ssh/authorized_keys" % (node)
                 
-                master.ssh.execute("scp ~disco/.ssh/id_rsa.pub %s:~disco/.ssh/authorized_keys" % (node))
-                master.ssh.execute("scp ~disco/.erlang.cookie %s:~disco/" % (node))
-                
-            
-            for node in nodes:
-                 node.ssh.execute("chown -R disco:disco ~disco")
-                 
-            master.ssh.execute("/etc/init.d/disco start")
+                master.ssh.execute("scp /root/.erlang.cookie %s:/root/" % (node))
+            master.ssh.execute("/opt/anaconda/bin/disco stop")
+            master.ssh.execute("/opt/anaconda/bin/disco start")
+
 
     def run(self, nodes, master, user, user_shell, volumes):
         for node in nodes:
             self.init_node(node, master)
         self.config_disco(master, nodes)
         self.config_anaconda(master,nodes);
-        
-        
+
+
     def on_add_node(self, node, nodes, master, user, user_shell, volumes):
         self.init_node(node, master)
-        
+
         # What happens to running jobs on a restart?
         self.config_disco(master, nodes)
         self.config_anaconda(master,nodes);
-        
+
 
     def stop_node(self, node):
         pass
-
     def on_remove_node(self, node, nodes, master, user, user_shell, volumes):
         self.stop_node(node)
 
